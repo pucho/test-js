ac1b29f6a0a0cfc557a3309740982875e71e8e69
Glitch (hello-webpage) <none>
2020-12-14 19:36:22 +0000

    🌱🎍 Checkpoint ./script.js:8796885/1675 ./index.html:8796885/461

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ const questionsWrapper = document.getElementById("questions");
 const buildQuiz = (questions) => {
   const output = [];
   
+  // search questions and create each fieldset
   let answerValue;
   questions.forEach((currentQuestion, questionNumber) => {
     const answers = [];
 
+    // iterate from each answer
     Object.keys(currentQuestion.answers).forEach((answerKey) => {
       answerValue = currentQuestion.answers[answerKey];
       
+      // create radio input to store the response
       answers.push(`
         <div class="form-check">
-          <input type="radio" class="form-check-input" value="${answerValue}" name="question${questionNumber}" id="answer${answerValue}" required>
+          <input type="radio" class="form-check-input" value="${answerValue}" name="question${questionNumber}" id="answer${answerValue}">
           <label class="form-check-label" for="answer${answerValue}">
             ${answerKey}: ${answerValue}
           </label>
@@ -22,18 +25,39 @@ const buildQuiz = (questions) => {
     })
     
     output.push(`
-      <div class="mb-2">
-        <strong>${currentQuestion.question}</strong>
-        ${answers.join("")}
-      </div>
+      <fieldset>
+        <div class="mb-3">
+          <strong>${currentQuestion.question}</strong>
+          <div class="answers">${answers.join("")}</div>
+        </div>
+      </fieldset>
     `);
   })
   
+  // build all questions
   questionsWrapper.innerHTML = output.join("");
 }
 
-const checkResults = () => {
+const checkResults = (e, questions) => {
   console.log("Checking results...")
+  e.preventDefault();
+  
+  const quizAnswers = document.querySelectorAll(".answers");
+  
+  questions.forEach((currentQuestion, questionNumber) => {
+    // find selected answers
+    const answersContainer = quizAnswers[questionNumber]
+    console.log("answersContainer", answersContainer)
+    const selector = `input[name="question${questionNumber}"]:checked`;
+    const answerValue = (answersContainer.querySelector(selector) || {}).value;
+    console.log("answerValue", answerValue)
+    
+    const isCorrect = answerValue ===  currentQuestion.correctAnswer;
+    
+    console.log("isCorrect", isCorrect);
+    
+    answersContainer.toggleClass("is-invalid", !isCorrect);
+  })
 }
 
 
@@ -70,4 +94,4 @@ const myQuestions = [
 
 buildQuiz(myQuestions);
 
-quizForm.addEventListener("submit", checkResults);
+quizForm.addEventListener("submit", (e) => checkResults(e, myQuestions));
