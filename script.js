b0883863fecf906b2fdb4fc13a9c3d7bd60ab85d
Glitch (hello-webpage) <none>
2023-03-02 14:33:09 +0000

    🌪🛩 Checkpoint ./script.js:40903233/202 ./index.html:40903233/11

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const myQuestions = [
   {
+    id: 1,
     question: "Who invented JavaScript?",
     answers: {
       a: "Douglas Crockford",
@@ -9,6 +10,7 @@ const myQuestions = [
     correctAnswer: "c",
   },
   {
+    id: 2,
     question: "Which one of these is a JavaScript package manager?",
     answers: {
       a: "Node.js",
@@ -18,6 +20,7 @@ const myQuestions = [
     correctAnswer: "c",
   },
   {
+    id: 3,
     question: "Which tool can you use to ensure code quality?",
     answers: {
       a: "Angular",
@@ -31,27 +34,25 @@ const myQuestions = [
 
 const questionsContainer = document.querySelector("#questions");
 
-myQuestions.forEach((question) => {
-  const questionNode = document.createElement("div");
+myQuestions.forEach((question, index) => {
+  const questionNode = document.createElement("ul");
   const questionTitle = document.createElement("h4");
   questionTitle.appendChild(document.createTextNode(question.question));
   let answersHTML = ``;
   for (const [key, value] of Object.entries(question.answers)) {
     answersHTML += `
     <li>
-      <input type="radio" id="${value}" name="${question.question}" value="${value}" required/>
+      <input type="radio" id="${key}-${question.id}" name="${question.id}" value="${value}" required/>
       <label for="${value}">${value}</label>
     </li>
     `;
   }
-  console.log(answersHTML)
   questionNode.innerHTML = answersHTML;
   questionNode.prepend(questionTitle)
   questionsContainer.appendChild(questionNode);
 });
 
 const form = document.querySelector("#quiz");
-console.log(form);
 form.addEventListener(
   "submit",
   (event) => {
@@ -62,14 +63,10 @@ form.addEventListener(
 );
 
 form.addEventListener("formdata", (e) => {
-  console.log("formdata fired");
-
   // Get the form data from the event object
   const data = e.formData;
   console.log(data);
   for (const value of data.values()) {
     console.log(value);
   }
-
-  // submit the data via XHR
 });
